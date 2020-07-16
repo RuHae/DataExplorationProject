96a3cfaf719cf005fdfd0b06785d59239f24617e
TheRealPython <51164126+TheRealPython@users.noreply.github.com>
2020-07-16 13:03:41 +0200

    Update run.py
    Adjust file path

--- Tensorflow_2_gpu_support/Code/run.py
+++ Tensorflow_2_gpu_support/Code/run.py
@@ -4,7 +4,7 @@ from preprocessing import load_training_images
 ##################################################################################################
 # Uncomment the following line before first code execution # run this python file to load images #
 ##################################################################################################
-#load_training_images(".\\breast-histopathology-images\IDC_regular_ps50_idx5")
+#load_training_images("..\\breast-histopathology-images\\IDC_regular_ps50_idx5") 
 
 
 print("Test started")
