6a5cb52e160d687c0ec40974af105122cb2375a9
TheRealPython <51164126+TheRealPython@users.noreply.github.com>
2020-07-16 12:49:02 +0200

    Update run.py

--- Tensorflow_2_gpu_support/Code/run.py
+++ Tensorflow_2_gpu_support/Code/run.py
@@ -1,4 +1,11 @@
 from model import run_training
+from preprocessing import load_training_images
+
+##################################################################################################
+# Uncomment the following line before first code execution # run this python file to load images #
+##################################################################################################
+#load_training_images(".\\breast-histopathology-images\IDC_regular_ps50_idx5")
+
 
 print("Test started")
 run_training()
